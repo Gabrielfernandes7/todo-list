b66aedc872bd6391838d9b7ec02e2449c13027c5
Gabriel Fernandes <78227127+Gabrielfernandes7@users.noreply.github.com>
2024-01-15 19:54:38 -0300

    docs: swagger (#2)

--- src/main/java/com/todo/todolist/controller/TodoController.java
+++ src/main/java/com/todo/todolist/controller/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.todo.todolist.entity.Todo;
 import com.todo.todolist.service.TodoService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 @RestController
@@ -26,21 +27,25 @@ public class TodoController {
         this.todoService = todoService;
     }
     
+    @Operation(summary = "Criar uma tarefa", description = "Cria uma nova tarefa")
     @PostMapping
     List<Todo> criarTarefa(@RequestBody @Valid Todo todo) {
         return todoService.criarTarefa(todo);
     }
 
+    @Operation(summary = "Mostrar tarefas", description = "Mostra todas tarefas")
     @GetMapping
     List<Todo> listarTarefas() {
         return todoService.listarTarefa();
     }
 
+    @Operation(summary = "Atualizar dados de tarefa", description = "Atualiza dados da tarefa")
     @PutMapping
     List<Todo> atualizarTarefa(Todo todo) {
         return todoService.atualizarTarefa(todo);
     } 
 
+    @Operation(summary = "Apagar tarefa", description = "Apaga tarefa")
     @DeleteMapping("{id}")
     List<Todo> apagarTarefa(@PathVariable("id") Long id) {
         return todoService.apagarTarefa(id);
